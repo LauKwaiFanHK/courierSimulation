1ceeab1f5cc1295caf044d0978df9f8847b2b6db
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2021-12-27 12:51:30 +0100

    plane agent makes the plane to stop after all stops on a route reached

--- src/main/java/airShipping2/PlaneAgent.java
+++ src/main/java/airShipping2/PlaneAgent.java
@@ -1,5 +1,7 @@
 package airShipping2;
 
+import java.util.ArrayList;
+
 import jadex.bridge.IInternalAccess;
 import jadex.commons.future.Future;
 import jadex.commons.future.IFuture;
@@ -24,8 +26,13 @@ public class PlaneAgent {
 	private final IVector2 airportC = new Vector2Double(8.2, 11.7);
 	private final IVector2 airportD = new Vector2Double(4.9, 1.1);
 
+	private ArrayList<IVector2> route = new ArrayList<>();
+
 	@OnInit
 	public IFuture<Void> agentInit() {
+		route.add(airportC);
+		route.add(airportA);
+		route.add(airportB);
 
 		ServiceQuery<IMapService> query = new ServiceQuery<IMapService>(IMapService.class);
 		query.setScope(ServiceScope.PLATFORM);
@@ -41,19 +48,25 @@ public class PlaneAgent {
 
 	@OnStart
 	public IFuture<Void> agentStart() {
-		IResultListener<Void> rl = new IResultListener<Void>() {
-			public void resultAvailable(Void done) {
 
-				System.out.println("Plane has arrived!");
-				IFuture<Void> arrived = mapService.setPlaneTarget(airportA);
-				arrived.addResultListener(this);
+		IResultListener<Void> rl = new IResultListener<Void>() {
+			int i = 0;
+
+			public void resultAvailable(Void done) {
+				i = (i + 1) % route.size();
+
+				if (!(i > route.size())) {
+					System.out.println("Plane has arrived at " + route.get(i - 1));
+					IFuture<Void> arrived = mapService.setPlaneTarget(route.get(i));
+					arrived.addResultListener(this);
+				}
 			}
 
 			public void exceptionOccurred(Exception e) {
 			}
 		};
 
-		IFuture<Void> arrived = mapService.setPlaneTarget(airportC);
+		IFuture<Void> arrived = mapService.setPlaneTarget(route.get(0));
 		arrived.addResultListener(rl);
 
 		return new Future<Void>();
