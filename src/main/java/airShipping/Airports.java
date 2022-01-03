c004db8a910fa061c676d51b4168c25484834515
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2022-01-03 13:39:57 +0100

    delete project old version

---
+++ src/main/java/airShipping/Airports.java
@@ -0,0 +1,23 @@
+package airShipping;
+
+import jadex.extension.envsupport.math.IVector2;
+import jadex.extension.envsupport.math.Vector2Double;
+
+public class Airports {
+	private final IVector2 airportA = new Vector2Double(1.7, 7.9);
+	private final IVector2 airportB = new Vector2Double(13.8, 2.5);
+	private final IVector2 airportC = new Vector2Double(8.2, 11.7);
+	private final IVector2 airportD = new Vector2Double(4.9, 1.1);
+	public IVector2 getAirportA() {
+		return airportA;
+	}
+	public IVector2 getAirportB() {
+		return airportB;
+	}
+	public IVector2 getAirportC() {
+		return airportC;
+	}
+	public IVector2 getAirportD() {
+		return airportD;
+	}
+}
---
+++ src/main/java/airShipping/IPlaneService.java
@@ -0,0 +1,14 @@
+package airShipping;
+
+import jadex.bridge.service.annotation.Service;
+import jadex.commons.future.IFuture;
+import jadex.extension.envsupport.math.IVector2;
+
+@Service
+public interface IPlaneService {
+
+	public IFuture<Void> setPlaneID(String planeID);
+
+//	public IFuture<Void> setStartPosition(IVector2 startPosition);
+
+}
